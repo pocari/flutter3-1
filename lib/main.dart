3a870fe8611c5857476fc6a2d54b0744f0e8c2ca
pocari <caffelattenonsugar@gmail.com>
2019-11-07 00:12:07 +0900

    add event lister

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,18 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _counter = 0;
 
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Flutter Demo@',
@@ -17,19 +29,33 @@ class _MyAppState extends State<MyApp> {
           primarySwatch: Colors.blue,
         ),
         home: Scaffold(
-          body: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times!:',
-              ),
-              Text(
-                '[ $_counter ]',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          )),
-        ),
+            body: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times!:',
+                ),
+                Text(
+                  '[ $_counter ]',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
+            )),
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            )),
       );
 }
