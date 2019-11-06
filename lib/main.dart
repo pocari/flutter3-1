0c58648e092312dbe6d656ed544fda66706a46bb
pocari <caffelattenonsugar@gmail.com>
2019-11-06 22:46:01 +0900

    hello, world

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo@',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              'Flutter Demo Home Page!!',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ),
+      );
+}
