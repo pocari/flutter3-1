06a333d47603305ceab9d89509884f605966018f
pocari <caffelattenonsugar@gmail.com>
2019-11-06 22:58:09 +0900

    add statefullwidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
+  @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'Flutter Demo@',
         theme: ThemeData(
@@ -10,11 +18,18 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           body: Center(
-            child: Text(
-              'Flutter Demo Home Page!!',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ),
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times!:',
+              ),
+              Text(
+                '[ $_counter ]',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ],
+          )),
         ),
       );
 }
